b5fbce58bf94d334bee1e4bb3e11c3fa1c296d06
jan-natt <jannatulferdous139997@gmail.com>
2025-08-20 09:33:56 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -18,37 +18,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test',function(){
-    return"test Route"; 
-});
-
-//Route::post('$uri', $callback);
-
-//Route::put('$uri', $callback);
-//Route::delete('$uri', $callback);
-//Route::patch('$uri', $callback);
-//Route::options('$uri', $callback);
-//Route::post('$uri', $callback);
-
-Route::group(['prefix' => 'Training'],function(){
-    
-
-    Route::get('/aws', function () {
-        echo 'aws traning';
-    });
-
-    Route::get('/laravel', function () {
-        echo 'laravel traning';
-    });
-
-});
-
-Route::get('/compact',function(){
-    $articleName = ['article 1' ,'article 2'];
-    $articlepublished = 'on my blog';
-
-    return view('testview',compact('articleName','articlepublished'));
-});
-
-
-Route::get('/myarticle', [PageController::class, 'article']);
