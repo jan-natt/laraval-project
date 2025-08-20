ab2d3eb40f67c093e9eb4b90cc71563ce1c686d6
jan-natt <jannatulferdous139997@gmail.com>
2025-08-20 12:53:22 +0600

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
     <div>
          <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-success" type="submit">Search</button>
+        <button class="btn btn-light text-secondary" type="submit">Search</button>
       </form>
     </div>
     
@@ -30,6 +30,11 @@
     </div>
   </div>
 </nav>
+<footer class="bg-primary text-light py-3 mt-5">
+    <div class="container text-center">
+        &copy; 2025 E-commerce. All rights reserved.
+    </div>
+</footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
   </body>
 </html>
