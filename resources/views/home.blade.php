a2f0735f1c3396fba54833eef501d2f20cf64b03
jan-natt <jannatulferdous139997@gmail.com>
2025-08-22 20:41:37 +0600

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -97,4 +97,61 @@
         </div>
     </div>
 </section>
+
+
+<!-- product -->
+ <section class="my-5">
+    <div class="container">
+        <h1 class="mb-4">Our top Products</h1>
+        <div class="row theme-product">
+            <div class="col-lg-3">
+                <div class="card" >
+                <a href="#"><img src="{{ asset('assets/images/1.jpg') }}" class="card-img-top" alt="..."></a>
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to
+                        build on the card title and make up the bulk of the card’s content.</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+             <div class="col-lg-3">
+                <div class="card" >
+                 <a href="#"><img src="{{ asset('assets/images/1.jpg') }}" class="card-img-top" alt="..."></a>
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to
+                        build on the card title and make up the bulk of the card’s content.</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+             <div class="col-lg-3">
+                <div class="card" >
+                 <a href="#"><img src="{{ asset('assets/images/1.jpg') }}" class="card-img-top" alt="..."></a>
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to
+                        build on the card title and make up the bulk of the card’s content.</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+             <div class="col-lg-3">
+                <div class="card" >
+                 <a href="#"><img src="{{ asset('assets/images/1.jpg') }}" class="card-img-top" alt="..."></a>
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to
+                        build on the card title and make up the bulk of the card’s content.</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
